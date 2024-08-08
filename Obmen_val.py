6438c4bbbc4f91f66476c26090584ce1e1774b25
HUAWEI <koshkalaska@yandex.ru>
2024-08-08 23:15:20 +1000

    в метку добавлены названия валют

--- Obmen_val.py
+++ Obmen_val.py
@@ -5,6 +5,12 @@ from tkinter import messagebox as mb
 from tkinter import ttk
 
 
+def update_c_label(event):#функция обновления метки,event  потому что вызывам в функции комбобоx силейт
+    code = combobox.get()
+    name = cur[code]
+    c_label.config(text=name)
+
+
 def exchange():#функция обмены валюты
     #code = entry.get()#получаем из поля ввода энтри что ввел пользователь
     code = combobox.get()
@@ -15,7 +21,8 @@ def exchange():#функция обмены валюты
             data = response.json()#раскладываем полученное в формате json  информацию в виде словаря
             if code in data['rates']:#если в дата существует введеный код
                 exchange_rate = data['rates'][code]# находим знгачение
-                mb.showinfo("Курс обмена", f"Курс: {exchange_rate:.2f} {code} за 1 доллар")#выводим пользователю
+                c_name = cur[code]
+                mb.showinfo("Курс обмена", f"Курс: {exchange_rate:.2f} {c_name} за 1 доллар")#выводим пользователю
             else:
                 mb.showerror("Ошибка", f"Валюта {code} не найдена")#ессли код валюты не найден выводим предупреждение
         except Exception as e:
@@ -24,15 +31,34 @@ def exchange():#функция обмены валюты
         mb.showwarning("Внимание!", "Введите код валюты!")#
 
 
+cur = {'RUB':'Российский рубль',
+       'EUR':'Евро',
+       'GBP':'Британский фунт стерлингов',
+       'JPY':'Японская йена',
+       'CNY':'Китайский юань',
+       'KZT':'Казахский тенге',
+       'UZS':'Узбекский сум',
+       'CHF':'Швейцарский франк',
+       'AED':'Дирхам ОАЭ',
+       'CAD':'Канадский доллар'
+       }
+
+
 window = Tk()
 window.title("Курсы обмена валют")
 window.geometry("360x180")
 
 Label(text="Выберите код валюты").pack(padx=10, pady=10)
 
-curses = ['RUB','RUR','GBP','JPY','CNY','KZT','UZS','CHF','AED','CAD']
-combobox = ttk.Combobox(values=curses)
+#cur = ['RUB','RUR','GBP','JPY','CNY','KZT','UZS','CHF','AED','CAD']
+
+combobox = ttk.Combobox(values=list(cur.keys()))
 combobox.pack(padx=10, pady=10)
+combobox.bind("<<ComboboxSelected>>", update_c_label)
+
+c_label = ttk.Label()
+c_label.pack(padx=10, pady=10)
+
 
 #entry = Entry()
 #entry.pack(padx=10, pady=10)
