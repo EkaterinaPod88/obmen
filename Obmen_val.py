d2183a18e861d9e9431b7763e52c13e0412582cf
HUAWEI <koshkalaska@yandex.ru>
2024-08-08 22:36:41 +1000

    Изменен формат вывода курса.Теперь выводится только 2 знака полсе запятой

--- Obmen_val.py
+++ Obmen_val.py
@@ -4,8 +4,23 @@ from tkinter import *
 from tkinter import messagebox as mb
 
 
-def exchange():
+def exchange():#функция обмены валюты
+    code = entry.get()#получаем из поля ввода энтри что ввел пользователь
 
+    if code:
+        try:
+            response = requests.get('http://open.er-api.com/v6/latest/USD')#делаем запрос к сайту
+            response.raise_for_status()#получаем информацию в response
+            data = response.json()#раскладываем полученное в формате json  информацию в виде словаря
+            if code in data['rates']:#если в дата существует введеный код
+                exchange_rate = data['rates'][code]# находим знгачение
+                mb.showinfo("Курс обмена", f"Курс: {exchange_rate:.2f} {code} за 1 доллар")#выводим пользователю
+            else:
+                mb.showerror("Ошибка", f"Валюта {code} не найдена")#ессли код валюты не найден выводим предупреждение
+        except Exception as e:
+            mb.showerror("Ошибка", f"Произошла ошибка: {e}.")#обрабатываем исключения
+    else:
+        mb.showwarning("Внимание!", "Введите код валюты!")#
 
 
 window = Tk()
@@ -17,6 +32,6 @@ Label(text="Введите код валюты").pack(padx=10, pady=10)
 entry = Entry()
 entry.pack(padx=10, pady=10)
 
-Button(text-"Получить курс обмена к доллору", command=exchange).pack(padx=10, pady=10)
+Button(text="Получить курс обмена к доллору", command=exchange).pack(padx=10, pady=10)
 
 window.mainloop()
